8b3cc01acd2482fc3a1b0d881862862837600a0b
iyarsius <iyarsius@gmail.com>
2024-11-07 17:59:25 +0100

    exported contract interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from "./structures/Client"
+export * from "./structures/contracts/UniswapV2Factory"
+export * from "./structures/contracts/UniswapV2Pair"
+export * from "./structures/contracts/UniswapV2Router02"
